8a6cb4a648d3347a0eaf7ff1954d39c806e85d8a
Luca Weiss <WEI16416@spengergasse.at>
2016-07-13 23:15:27 +0200

    ?

--- docker-setup.sh
+++ docker-setup.sh
@@ -6,5 +6,5 @@ echo "Pulling down docker image."
 sudo docker pull jftr/fairphone2-build-env
 
 echo "Running docker image."
-sudo docker run --rm --net=host -v /home/jenkins/workspace/Kernel/:/var/fairphone_os/ --entrypoint='/bin/bash -c "if [ -d fairphone_jenkins/ ]; then git pull; else git clone https://github.com/z3ntu/fairphone_jenkins; fi; ./fairphone_jenkins/build-kernel.sh"' jftr/fairphone2-build-env 
-
+#sudo docker run --rm --net=host -v /home/jenkins/workspace/Kernel/:/var/fairphone_os/ --entrypoint='/bin/bash -c "if [ -d fairphone_jenkins/ ]; then git -C fairphone_jenkins/ pull; else git clone https://github.com/z3ntu/fairphone_jenkins; fi; ./fairphone_jenkins/build-kernel.sh"' jftr/fairphone2-build-env 
+sudo docker run --rm --net=host -v /home/jenkins/workspace/Kernel/:/var/fairphone_os/ jftr/fairphone2-build-env /bin/bash -c "if [ -d fairphone_jenkins/ ]; then git -C fairphone_jenkins/ pull; else git clone https://github.com/z3ntu/fairphone_jenkins; fi; ./fairphone_jenkins/build-kernel.sh"
