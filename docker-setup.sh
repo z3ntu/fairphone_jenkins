461ed26ea9478a6e93629bcd3da22293b94bb90c
Luca Weiss <WEI16416@spengergasse.at>
2016-07-13 22:51:56 +0200

    update

--- docker-setup.sh
+++ docker-setup.sh
@@ -2,5 +2,9 @@
 
 echo "Starting Docker..."
 
+echo "Pulling down docker image."
+sudo docker pull jftr/fairphone2-build-env
+
+echo "Running docker image."
 sudo docker run --rm --net=host -v /home/jenkins/workspace/Kernel/:/var/fairphone_os/ -i jftr/fairphone2-build-env /bin/bash -c "if [ -d fairphone_jenkins/ ]; then git pull; else git clone https://github.com/z3ntu/fairphone_jenkins; fi; ./fairphone_jenkins/build-kernel.sh"
 
