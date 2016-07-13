915e3d097d444020a01d855172f76d6f4f601cc2
Luca Weiss <WEI16416@spengergasse.at>
2016-07-13 22:48:13 +0200

    add message

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "Starting Docker..."
+
 sudo docker run --rm --net=host -v /home/jenkins/workspace/Kernel/:/var/fairphone_os/ -i jftr/fairphone2-build-env /bin/bash -c "if [ -d fairphone_jenkins/ ]; then git pull; else git clone https://github.com/z3ntu/fairphone_jenkins; fi; ./fairphone_jenkins/build-kernel.sh"
 
