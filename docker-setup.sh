877009142206715b7a9fc671ef7ec4513b4d2335
Luca Weiss <WEI16416@spengergasse.at>
2016-07-13 21:41:56 +0200

    fix docker

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run --rm --net=host -v /home/jenkins/workspace/Kernel/:/var/fairphone_os/ -i -t jftr/fairphone2-build-env /bin/bash -c "curl -O https://raw.githubusercontent.com/z3ntu/fairphone_jenkins/master/build-kernel.sh && chmod +x build-kernel.sh && ./build-kernel.sh"
+sudo docker run --rm --net=host -v /home/jenkins/workspace/Kernel/:/var/fairphone_os/ -i jftr/fairphone2-build-env /bin/bash -c "curl -O https://raw.githubusercontent.com/z3ntu/fairphone_jenkins/master/build-kernel.sh && chmod +x build-kernel.sh && ./build-kernel.sh"
