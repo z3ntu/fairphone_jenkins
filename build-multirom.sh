d312822145be281accdb04690fd9daf7f20df4f1
Luca Weiss <WEI16416@spengergasse.at>
2016-07-15 23:40:57 +0200

    fix multirom end copy

--- build-multirom.sh
+++ build-multirom.sh
@@ -6,9 +6,12 @@ cd /var/fairphone_os/android
 
 make -j10 multirom_zip
 
-if [ ! -f out/target/product/FP2/multirom* ]; then
+if [ ! -f out/target/product/FP2/multirom-*-UNOFFICIAL-FP2.zip ];
         echo "Compilation failed."
 else
         echo "Compilation finished"
-        cp out/target/product/FP2/multirom* /var/out/
+	filename=$(basename out/target/product/FP2/multirom-*-UNOFFICIAL-FP2.zip)
+	echo "Multirom $filename is finished!"
+	cp out/target/product/FP2/$filename /var/out/
+        cp out/target/product/FP2/$filename.md5sum /var/out/
 fi
