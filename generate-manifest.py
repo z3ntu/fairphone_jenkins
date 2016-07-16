87a72cb2c12b2d7c8539bfae8cbe2dd2f4928ec6
Luca Weiss <WEI16416@spengergasse.at>
2016-07-16 17:22:18 +0200

    finish manifest script

--- generate-manifest.py
+++ generate-manifest.py
@@ -4,14 +4,18 @@ from datetime import datetime
 import hashlib
 import json
 import os
+import sys
 
 url_base = "https://raw.githubusercontent.com/z3ntu/multirom_fairphone_fp2/master/"
 
-mr_filename = "multirom-20160716-v33-UNOFFICIAL-FP2.zip"
-mr_version = "33"
-rec_filename = "TWRP_3.0.0-0_multirom_FP2_20160716-02.img"
-rec_version = "mrom20160716-02"
-uninstaller = "multirom_uninstaller.zip"
+#mr_filename = "/home/jenkins/multirom_fairphone_fp2/installer/multirom-20160716-v33-UNOFFICIAL-FP2.zip"
+#rec_filename = "/home/jenkins/multirom_fairphone_fp2/recovery/TWRP_3.0.0-0_multirom_FP2_20160716-02.img"
+mr_filename = sys.argv[1]
+rec_filename = sys.argv[2]
+uninstaller = "/home/jenkins/multirom_fairphone_fp2/uninstaller/multirom_uninstaller.zip"
+
+mr_version = mr_filename.split("-")[2].replace("v", "")
+rec_version = "mrom" + rec_filename.split("_")[4].split(".")[0]
 
 manifest = {}
 
@@ -22,8 +26,8 @@ manifest['gpg'] = False
 manifest['devices'] = []
 devices = []
 files = []
-files.append({"url": url_base + "installer/" + mr_filename, "version": mr_version, "size": os.path.getsize(mr_filename), "md5": hashlib.md5(open(mr_filename, "rb").read()).hexdigest()})
-files.append({"url": url_base + "recovery/" + rec_filename, "version": rec_version, "size": os.path.getsize(rec_filename), "md5": hashlib.md5(open(rec_filename, "rb").read()).hexdigest()})
+files.append({"url": url_base + "installer/" + os.path.basename(mr_filename), "version": mr_version, "size": os.path.getsize(mr_filename), "md5": hashlib.md5(open(mr_filename, "rb").read()).hexdigest()})
+files.append({"url": url_base + "recovery/" + os.path.basename(rec_filename), "version": rec_version, "size": os.path.getsize(rec_filename), "md5": hashlib.md5(open(rec_filename, "rb").read()).hexdigest()})
 files.append({"url": url_base + "uninstaller/multirom_uninstaller.zip", "version": "", "size": os.path.getsize(uninstaller), "md5": hashlib.md5(open(uninstaller, "rb").read()).hexdigest()})
 ubuntu_touch = {"req_multirom": "33", "req_recovery": "mrom20160716-01"}
 devices.append({"name": "FP2", "files": files, "ubuntu_touch": ubuntu_touch})
--- update-manifest.sh
+++ update-manifest.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-basepath="/home/jenkins/workspace/out"
+echo "Updating multirom repo."
 
-echo "updating manifest"
-multirom_filename=$(ls -t $basepath/multirom-*-UNOFFICIAL-FP2.zip | head -1)
-recovery_filename=$(ls -t $basepath/TWRP_*_multirom_FP2_*.img | head -1)
+outpath="/home/jenkins/workspace/out"
+repopath="/home/jenkins/multirom_fairphone_fp2"
 
-echo $multirom_filename $recovery_filename
+if [ ! -d $repopath ]; then
+	git clone git@multirom.github.com:z3ntu/multirom_fairphone_fp2.git $repopath
+fi
+
+multirom_filename=$(ls -t $outpath/multirom-*-UNOFFICIAL-FP2.zip | head -1)
+recovery_filename=$(ls -t $outpath/TWRP_*_multirom_FP2_*.img | head -1)
+kernel_filename=$(ls -t $outpath/fairphone-2-kernel-*.img | head -1)
+# TODO Also add uninstaller and possibly the kernel (if I get to know how to package that)
+
+echo "Cleaning $repopath/installer/"
+rm $repopath/installer/*
+echo "Copying $(basename $multirom_filename) to repo."
+cp $multirom_filename $repopath/installer
+
+echo "Cleaning $repopath/recovery/"
+rm $repopath/recovery/*
+echo "Copying $(basename $recovery_filename) to repo."
+cp $recovery_filename $repopath/recovery
+
+echo "Cleaning $repopath/kernel/"
+rm $repopath/kernel/*
+echo "Copying $(basename $kernel_filename) to repo."
+cp $kernel_filename $repopath/kernel
+
+manifest_content=$(python generate-manifest.py $multirom_filename $recovery_filename)
+echo -n $manifest_content > "$repopath/manifest.json"
+
+echo "Updated manifest. Committing and pushing!"
+git -C $repopath add $repopath
+git -c "user.name=Jenkins (z3ntu)" -c "user.email=noreply@z3ntu.xyz" -C $repopath commit -m "MultiROM FP2"
+git -C $repopath push
