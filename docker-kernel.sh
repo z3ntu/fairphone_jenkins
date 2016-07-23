b309112a6bec2dcfabc90ca0c4b75731a6f6cc96
Luca Weiss <WEI16416@spengergasse.at>
2016-07-23 19:12:20 +0200

    change to my vim docker image

--- docker-kernel.sh
+++ docker-kernel.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+DOCKER_IMAGE="z3ntu/fairphone2-build-env-with-vim"
+
 echo "Starting Docker..."
 
 echo "Pulling down docker image."
-sudo docker pull jftr/fairphone2-build-env
+sudo docker pull $DOCKER_IMAGE
 
 echo "Running docker image."
 mkdir -p /home/jenkins/workspace/common/
 
-sudo docker run --rm --net=host -v /home/jenkins/workspace/common/:/var/fairphone_os/ -v /home/jenkins/workspace/out/:/var/out/ jftr/fairphone2-build-env /bin/bash -c "if [ -d fairphone_jenkins/ ]; then git -C fairphone_jenkins/ pull; else git clone https://github.com/z3ntu/fairphone_jenkins; fi; ./fairphone_jenkins/build-kernel.sh"
+sudo docker run --rm --net=host -v /home/jenkins/workspace/common/:/var/fairphone_os/ -v /home/jenkins/workspace/out/:/var/out/ $DOCKER_IMAGE /bin/bash -c "if [ -d fairphone_jenkins/ ]; then git -C fairphone_jenkins/ pull; else git clone https://github.com/z3ntu/fairphone_jenkins; fi; ./fairphone_jenkins/build-kernel.sh"
--- docker-manual.sh
+++ docker-manual.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+DOCKER_IMAGE="z3ntu/fairphone2-build-env-with-vim"
+
 echo "Starting Docker..."
 
 echo "Pulling down docker image."
-sudo docker pull jftr/fairphone2-build-env
+sudo docker pull $DOCKER_IMAGE
 
 echo "Running docker image."
 mkdir -p /home/jenkins/workspace/common/
 
-sudo docker run --rm --net=host -v /home/jenkins/workspace/common/:/var/fairphone_os/ -v /home/jenkins/workspace/out/:/var/out/ -i -t jftr/fairphone2-build-env /bin/bash
+sudo docker run --rm --net=host -v /home/jenkins/workspace/common/:/var/fairphone_os/ -v /home/jenkins/workspace/out/:/var/out/ -i -t $DOCKER_IMAGE /bin/bash
