dd0609d6ff7d26ca8766468ca478775f6d03a87e
Luca Weiss <luca@z3ntu.xyz>
2016-11-21 23:03:35 +0100

    moduralize efidroid script

--- efidroid/build-uefi.sh
+++ efidroid/build-uefi.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "Please provide the target device like \"fairphone/fp2\"!"
+    exit 1
+fi
+
+DEVICE=$1
+
+cd ../ # build outside of the jenkins build environment that we have one common folder
 if [ ! -d "efidroid/" ]; then
 	mkdir efidroid/
 	cd efidroid/
@@ -8,14 +16,17 @@ if [ ! -d "efidroid/" ]; then
 	mkdir -p .repo/local_manifests
 	ln -s ../manifests/optional.xml .repo/local_manifests/optional.xml
 	repo sync -j4 -c -f
-
-	echo "Add device tree"
-	git clone https://github.com/z3ntu/device -b fairphone/fp2 device/fairphone/fp2
 else
 	cd efidroid/
 	echo "Pulling new changes"
 	repo sync -j4 -c -f
-	git -C device/fairphone/fp2 pull
+fi
+
+if [ ! -d "device/$DEVICE" ]; then
+	echo "Add device tree"
+	git clone https://github.com/efidroid/device -b $DEVICE device/$DEVICE
+else
+	git -C device/$DEVICE pull
 fi
 
 echo "Cleaning up"
@@ -25,11 +36,13 @@ source build/envsetup.sh
 
 # I use these variables instead of the command "lunch"
 export BUILDTYPE=RELEASE
-export DEVICEID=fairphone/fp2
+export DEVICEID=$DEVICE
 
 make -j9 uefi
 make -j9 otapackage
-cp out/target/fairphone/fp2/otapackage-*-fairphone_fp2.zip /var/out/
+DEVICE_UNDERSCORE=${DEVICE/\//_}
+cp out/target/$DEVICE/otapackage-*-$DEVICE_UNDERSCORE.zip /var/out/
+cp out/target/$DEVICE/otapackage-*-$DEVICE_UNDERSCORE.zip ../$DEVICE_UNDERSCORE/
 
 #echo "Cleaning up"
 #make -j4 distclean
